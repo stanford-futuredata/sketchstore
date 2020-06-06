a63b166ec7672cefdbc63d589fb3d238412ccc25
Edward Gan <edgan8@gmail.com>
2020-06-06 13:48:54 -0700

    big zipf generato

--- java/src/main/java/runner/MicroBench.java
+++ java/src/main/java/runner/MicroBench.java
@@ -1,9 +1,19 @@
 package runner;
 
+import org.apache.commons.math3.distribution.ZipfDistribution;
+import org.apache.commons.math3.random.MersenneTwister;
+import org.apache.commons.math3.random.RandomAdaptor;
+import org.apache.commons.math3.random.RandomGenerator;
 import org.eclipse.collections.api.map.primitive.MutableLongDoubleMap;
 import org.eclipse.collections.impl.list.mutable.primitive.IntArrayList;
 import org.eclipse.collections.impl.map.mutable.primitive.LongDoubleHashMap;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -11,8 +21,31 @@ public class MicroBench
 {
   public static void main(String[] args) throws Exception {
 //      CoopRuntimeBench.prepare();
-    CoopRuntimeBench.runSB();
+//    CoopRuntimeBench.runSB();
 //    CoopRuntimeBench.run();
+      generateZipf();
+  }
+
+  public static void generateZipf() throws IOException {
+    RandomGenerator rng = new MersenneTwister(0);
+    ZipfDistribution dist = new ZipfDistribution(rng, 100_000_000, 1.1);
+    int chunkSize = 100_000_000;
+    int numChunks = 10;
+
+    String dirPath = "datasets/zipfbig";
+    Timer chunkTime = new Timer();
+    for (int chunkNum = 0; chunkNum < numChunks; chunkNum++) {
+      chunkTime.reset();
+      chunkTime.start();
+      Path filePath = Paths.get(dirPath, "zipf-" + chunkNum + ".csv");
+      PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(filePath.toString())));
+      for (int i = 0; i < chunkSize; i++) {
+        out.println(dist.sample());
+      }
+      out.close();
+      chunkTime.end();
+      System.out.println("chunk took: "+chunkTime.getTotalMs());
+    }
   }
 
   public static void arrayBench() {
