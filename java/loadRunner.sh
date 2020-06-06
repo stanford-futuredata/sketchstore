beceb15ddd7d6ed0231f7b3f8c4f5f1157e25b77
Edward Gan <edgan8@gmail.com>
2020-06-06 04:20:30 +0000

    plotting scripts

--- java/loadRunner.sh
+++ java/loadRunner.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -Xmx12g -Xms12g -cp target/sketchstore-1.0-SNAPSHOT.jar:$(cat cp.txt) runner.LoadRunner $@
+java -Xmx120g -Xms120g -cp target/sketchstore-1.0-SNAPSHOT.jar:$(cat cp.txt) runner.LoadRunner $@
